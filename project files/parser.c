26b7615408c19a379882661b607469372bf208c6
fiftyfivebells <stephen.l.bell@gmail.com>
2018-01-27 08:35:25 -0500

    Create functions to work with and print lvals

--- project files/parser.c
+++ project files/parser.c
@@ -35,6 +35,44 @@ typedef struct {
 
 enum { LVAL_NUM, LVAL_ERR };
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+// number type lval
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+
+// error type lval
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+}
+
+void lval_print(lval v) {
+    switch(v.type) {
+        case LVAL_NUM: printf("%li", v.num);
+        break;
+
+        case LVAL_ERR:
+            if (v.err == LERR_DIV_ZERO) {
+                printf("Error: you can't divide by zero!");
+            }
+            if (v.err == LERR_BAD_OP) {
+                printf("That's a bad operator, don't use it!");
+            }
+            if (v.err == LERR_BAD_NUM) {
+                printf("Bad number, shouldn't have used that!");
+            }
+        break;
+    }
+}
+
+// prints an lval, but with a newline after
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
 int main(int argc, char** argv) {
 
     // parsers
