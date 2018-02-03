d35dd87ea8dc5a732e3d5fa91274b5bfb58aa9b7
fiftyfivebells <stephen.l.bell@gmail.com>
2018-02-02 21:02:55 -0500

    Create helper function to add a builtin to the environment

--- project files/parser.c
+++ project files/parser.c
@@ -575,6 +575,33 @@ lval* builtin_pow(lenv* e, lval* a) {
     return builtin_op(e, a, "^");
 }
 
+void lenv_add_builtin(lenv* e, char* name, lbuiltin func) {
+    lval* k = lval_sym(name);
+    lval* v = lval_fun(func);
+    lenv_put(e, k, v);
+    lval_del(k)); lval_del(v);
+}
+
+void lenv_add_builtins(lenv* e) {
+    // list functions
+    lenv_add_builtin(e, "head", builtin_head);
+    lenv_add_builtin(e, "tail", builtin_tail);
+    lenv_add_builtin(e, "list", builtin_list);
+    lenv_add_builtin(e, "eval", builtin_eval);
+    lenv_add_builtin(e, "join", builtin_join);
+    lenv_add_builtin(e,  "len", builtin_len);
+    lenv_add_builtin(e, "init", builtin_init);
+    lenv_add_builtin(e, "cons", builtin_cons);
+
+    // arithmetic functions
+    lenv_add_builtin(e, "+", builtin_add);
+    lenv_add_builtin(e, "-", builtin_sub);
+    lenv_add_builtin(e, "*", builtin_mul);
+    lenv_add_builtin(e, "/", builtin_div);
+    lenv_add_builtin(e, "%", builtin_mod);
+    lenv_add_builtin(e, "^", builtin_pow);
+}
+
 lval* lval_eval_sexpr(lenv* e, lval* v) {
 
     // evaluate children
