40decc071f45da13ab58e3f185da0e45d89c9fd0
fiftyfivebells <stephen.l.bell@gmail.com>
2018-02-12 21:25:14 -0500

    Add lenv_copy function

--- project files/parser.c
+++ project files/parser.c
@@ -299,6 +299,21 @@ lenv* lenv_new(void) {
     return e;
 }
 
+// copy an environment
+lenv* lenv_copy(lenv* e) {
+    lenv* n = malloc(sizeof(lenv));
+    n->par = e->par;
+    n->count = e->count;
+    n->syms = malloc(sizeof(char*) * n->count);
+    n->vals = malloc(sizeof(lval*) * n->count);
+    for (int i = 0; i < e->count; i++) {
+        n->syms[i] = malloc(strlen(e->syms[i]) + 1);
+        strcpy(n->syms[i], e->syms[i]);
+        n->vals[i] = lval_copy(e->vals[i]);
+    }
+    return n;
+}
+
 void lval_del(lval* v);
 
 // delete an environment
